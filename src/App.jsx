0a55bc10c72ed2ea22c2e6115dbd1deb97c1a59c
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-07-20 16:21:30 +0530

    Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage()
-       getLocalStorage()
-  },)
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email,password)=>{
+    if (email == 'admin@me.com' && password == '123') {
+         console.log("This is Admin");
+         
+    }else if(email == 'user@me.com' && password == '123'){
+          console.log("This is user");
+          
+    }else{
+      alert("Invalid Credentials")
+    }
+    
+  }
+
+  
 
   return (
     <>
-      {<Login />}
+      {!user ? <Login handleLogin={handleLogin} />: ''}
       {/*<EmployeeDashboard />*/}
       {/*<AdminDashboard />*/}
     </>
