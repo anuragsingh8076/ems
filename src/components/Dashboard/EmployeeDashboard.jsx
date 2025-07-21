4ca0a8f02248386e86cfd275617a94eacd153af5
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-07-21 17:36:55 +0530

    separating user data and admin data

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -4,14 +4,15 @@ import TaskListNumbers from '../other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
 
-const EmployeeDashboard = () => {
-
+const EmployeeDashboard = ({data}) => {
+   
+   
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
         
-        <Header />
-        <TaskListNumbers />
-        <TaskList />
+        <Header data={data} />
+        <TaskListNumbers data={data} />
+        <TaskList data={data} />
     </div>
   )
 }
