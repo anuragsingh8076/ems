2078f7de9023fab760d5538a821c7400e5abbcd4
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-07-23 16:11:59 +0530

    Creating logout Features and more

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { AuthContext } from './context/AuthProvider'
+import { data } from 'autoprefixer'
 
 
 const App = () => {
@@ -11,16 +12,16 @@ const App = () => {
   const [loggedInUserData, setloggedInUserData] = useState(null)
   const authData = useContext(AuthContext)
 
-  // useEffect(() => {
+  useEffect(() => {
+        const loggedInUser = localStorage.getItem("loggedInUser")
+        if (loggedInUser) {
+          const userData = JSON.parse(loggedInUser)
+          setUser(userData.role)
+          setloggedInUserData(userData.data)
+        }
+      
 
-  //     if (authData) {
-  //       const loggedInUser = localStorage.getItem("loggedInUser")
-  //       if (loggedInUser) {
-  //         setUser(loggedInUser.role)
-  //       }
-  //     }
-
-  // }, [authData])
+  }, [])
 
   const handleLogin = (email,password)=>{
     if (email == 'admin@me.com' && password == '123') {
@@ -33,7 +34,7 @@ const App = () => {
         if (employee) {
           setUser('employee')
           setloggedInUserData(employee)
-          localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
+          localStorage.setItem('loggedInUser', JSON.stringify({role:'employee', data:employee}))
         }
             
     }else{
